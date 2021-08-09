4905dc29115bf554196ba40575ab4891626780f1
Abebe Kumelachew <abebe@gmail.com>
2021-08-09 03:47:10 +0300

    commited

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-
 const config = require('./common/config/env.config');
 const bodyParser = require('body-parser');
-//const AuthorizationRouter = require('./authorization/routes.config')
+const express = require('express');
+const app = express();
+
 const authRouter = require('./authorization/routes.config');
 const userRouter = require('./users/routes.config');
 const roleRouter = require('./privilages/Role/routes.config');
 const resourceRouter = require('./privilages/Resource/routes.config');
 const rightRouter = require('./privilages/Resource/routes.config');
-//const authorizationRouter = require('./authorization/routes.config');
+
 const supplierRouter = require('./suppliers/routes/supplierroute');
 const purchaseorderRouter = require('./purchaseorders/routes/purchaseorderroute');
 const saleorderRouter = require('./saleorders/routes/saleorderroute');
 const billRouter = require('./bills/routes/billroute');
 const invoiceRouter = require('./invoices/routes/invoiceroute');
-const express = require('express');
-const app = express(); 
+
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -32,23 +32,20 @@ app.use(function (req, res, next) {
 app.use(bodyParser.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-//authRouter.routesConfig(app);
-app.use('/auth', authRouter); //register auth routes
+
+/*app.use('/auth', authRouter); //register auth routes
 app.use('/user', userRouter); //register users routes
 app.use('/role', roleRouter); //register role routes
 app.use('/resource', resourceRouter); //register resource routes
-app.use('/right', rightRouter); //register resource routes
+app.use('/right', rightRouter); //register resource routes  */
 
-app.use('/supplier', supplierRouter);
-app.use('/purchaseorder', purchaseorderRouter);
+//app.use('/supplier', supplierRouter);
+/*app.use('/purchaseorder', purchaseorderRouter);
 app.use('/saleorder', saleorderRouter);
 app.use('/bill', billRouter);
-app.use('/invoice', invoiceRouter);
-
-//AuthorizationRouter.routesConfig(app);
-//userRouter.routesConfig(app);
+app.use('/invoice', invoiceRouter);*/
 
 
-app.listen(config.port,()=>{
+app.listen(config.port,()=> {
     console.log('app listening on port ',config.port);
 });
